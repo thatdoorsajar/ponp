53a9a141bc78f56ce90a95317a4f7d7f7422362d
thatdoorsajar <jgpearson1@gmail.com>
2017-07-23 10:39:50 +0100

    pushing to production

--- resources/assets/js/components/PopalQuizContent.vue
+++ resources/assets/js/components/PopalQuizContent.vue
@@ -1,32 +1,25 @@
 <template>
-    <div class="has-text-centered">
-        <div v-show="!eagerBeaver">
-            <img src="/img/francis-thumbs-up.jpg"
-                alt="Pope Francis Thumbs Up"
-                height="300px"
-                v-show="!flippinIt">
-            <img src="/img/francis-flippin-bird.jpg"
-                alt="Pope Francis Flippin It"
-                height="300px"
-                v-show="flippinIt">
-            <section class="section">
-                <div class="container has-text-centered">
-                    <h2 class="title is-2 show-pointer" @click="showEagerBeaver">COMING SOON!</h2>
-                </div>
-            </section>
+    <section class="section pt-0">
+        <div class="container has-text-centered">
+            <div v-show="!eagerBeaver">
+                <img src="/img/francis-thumbs-up.jpg"
+                    alt="Pope Francis Thumbs Up"
+                    v-show="!flippinIt"
+                    class="img-h300 py-1">
+                <img src="/img/francis-flippin-bird.jpg"
+                    alt="Pope Francis Flippin It"
+                    v-show="flippinIt"
+                    class="img-h300 py-1">
+                <h2 class="title is-2 pt-2 show-pointer" @click="showEagerBeaver">COMING SOON!</h2>
+            </div>
+            <div v-show="eagerBeaver">
+                <img src="/img/francis-very-happy.jpg"
+                    alt="Pope Francis Very Happy"
+                    class="img-h300 py-1">
+                <h2 class="title is-2 pt-2">Francis can't wait ;-)</h2>
+            </div>
         </div>
-        <div v-show="eagerBeaver">
-            <img src="/img/francis-very-happy.jpg"
-                alt="Pope Francis Thumbs Up"
-                height="300px"
-                v-show="!flippinIt">
-            <section class="section">
-                <div class="container has-text-centered">
-                    <h2 class="title is-2">Francis can't wait ;-)</h2>
-                </div>
-            </section>
-        </div>
-    </div>
+    </section>
 </template>
 
 <script>
